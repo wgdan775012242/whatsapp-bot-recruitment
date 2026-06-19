3d1f0636f066ac1cf2fd9d59fe7e82428e19dcfd
wgdan775012242 <wwrr11845@gmail.com>
2026-06-19 07:08:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/webhook', async (req, res) => {
             const userText = message.text.body;
 
             // إعداد Gemini
-            const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+            const model = genAI.getGenerativeModel({ model: "model: "gemini-pro" });
             const prompt = `أنت مساعد ذكي متخصص في التوظيف والسفر للسعودية. أجب باختصار واحترافية على: ${userText}`;
             
             const result = await model.generateContent(prompt);
